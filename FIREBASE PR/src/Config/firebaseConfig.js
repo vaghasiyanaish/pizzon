eeee746c2d92912907b7c2610be7a2dbf9e4d812
vaghasiyanaish <anishvaghasiya001@gmail.com>
2025-11-07 11:16:06 +0530

    amazonfirebase

--- FIREBASE PR/src/Config/firebaseConfig.js
+++ FIREBASE PR/src/Config/firebaseConfig.js
@@ -1,6 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAohkJfuxzwSty6xDsPemVU31XZak1Xr3g",
@@ -14,4 +13,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
